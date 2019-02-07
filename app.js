550bf44efa169a207bd99eaf9751a207b555f4fb
LeCrabe <gilles.gaubert@rocketmail.com>
2019-02-07 21:17:48 +0100

    maintenant chacun joue a son tour

--- app.js
+++ app.js
@@ -16,7 +16,12 @@ var idConnection=0;
 var listeConnectes=[];
 var nom="";
 var messBroad;
+// flag pour savoir a qui c est le tour :
+// au debut id 0 joue et pas id 1
+var tourId=[true,false];
 
+
+console.log(tourId[0]+"  "+tourId[1]);
 
 io.sockets.on('connection', function (socket, pseudo) {
     // on verifie le nombre de connection
@@ -32,11 +37,19 @@ io.sockets.on('connection', function (socket, pseudo) {
             socket.pseudo = pseudo;
             listeConnectes[idConnection]=pseudo;
             console.log(listeConnectes);
+            /*if (idConnection==0) {socket.monTour=true;}
+            if (idConnection==1) {socket.monTour=false;}*/
+            socket.monId=idConnection;
 
             for (var i=0;i<listeConnectes.length;i++) {
                 console.log("element "+i+" = "+listeConnectes[i]);
                 messBroad+=listeConnectes[i]+" ";
             }
+            
+            // ajoute info sur tour de jouer
+            if (socket.monTour) messBroad+=", a vous de jouer !";
+            else messBroad+=", pas à vous de jouer !";
+
             console.log(messBroad);
             
             //socket.emit('messageBroadcast', "vous :"+pseudo+" etes bien connectés");
@@ -49,17 +62,30 @@ io.sockets.on('connection', function (socket, pseudo) {
 
     	// Dès qu'on reçoit une "attaque" (clic sur le bouton), on le note dans la console
     	socket.on('addAttaque', function (coords) {
-        	// On récupère le pseudo de celui qui a cliqué dans les variables de session
-        	console.log(socket.pseudo+' joue : '+coords.abs+', '+coords.ord);
+        	console.log("socket.monId = "+socket.monId);
+        	if ((tourId[socket.monId]) && (listeConnectes.length==2)) {
+        		// c est bien ton tour donc c cool
+        		// On récupère le pseudo de celui qui a cliqué dans les variables de session
+        		console.log(socket.pseudo+' joue : '+coords.abs+', '+coords.ord);
+        		
+        		// on inverse les roles
+        		if (tourId[0]) {
+        			tourId[0]=false;
+        			tourId[1]=true;
+        		} else {
+        			tourId[0]=true;
+        			tourId[1]=false;
+        		}
+    		} else {
+    			// c est pas a toi de jouer donc je t envoie bouler
+    			console.log(socket.pseudo+ ' a joué mais ce n etait pas le moment !');
+    		}
     	});
     } else {
     	// il y a plus de 2 participants
         console.log("Pas plus de 2 connectés à la fois !!!");
     	socket.emit('messageConnection', '2 clients deja connectés au serveur...');
-
-    	
     }
 });
-
 
 server.listen(8080);
